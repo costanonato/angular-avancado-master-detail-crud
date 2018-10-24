3b73f0bedf681fcc09cc9cbfb375cc17128920e8
Costa Nonato <costa@nonato.com>
2018-10-24 19:08:32 -0300

    adding category service

---
+++ src/app/pages/categories/shared/category.service.ts
@@ -0,0 +1,78 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+
+import { Observable, throwError } from "rxjs";
+import { map, catchError, flatMap } from "rxjs/operators";
+
+import { Category } from "./category.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  private apiPath: string = "api/categories";
+
+  constructor(private http: HttpClient) { }
+
+
+  getAll(): Observable<Category[]> {
+    return this.http.get(this.apiPath).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToCategories)
+    )
+  }
+
+  getById(id: number): Observable<Category> {
+    const url = `${this.apiPath}/${id}`;
+
+    return this.http.get(url).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToCategory)
+    )
+  }
+
+  create(category: Category): Observable<Category> {
+    return this.http.post(this.apiPath, category).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToCategory)
+    )
+  }
+
+  update(category: Category): Observable<Category> {
+    const url = `${this.apiPath}/${category.id}`;
+
+    return this.http.put(url, category).pipe(
+      catchError(this.handleError),
+      map(() => category)
+    )
+  }
+
+  delete(id: number): Observable<any> {
+    const url = `${this.apiPath}/${id}`;
+
+    return this.http.delete(url).pipe(
+      catchError(this.handleError),
+      map(() => null)
+    )
+  }
+
+
+
+  // PRIVATE METHODS
+
+  private jsonDataToCategories(jsonData: any[]): Category[] {
+    const categories: Category[] = [];
+    jsonData.forEach(element => categories.push(element as Category));
+    return categories;
+  }
+
+  private jsonDataToCategory(jsonData: any): Category {
+    return jsonData as Category;
+  }
+
+  private handleError(error: any): Observable<any>{
+    console.log("ERRO NA REQUISIÇÃO => ", error);
+    return throwError(error);
+  }
+}
