635d29a1d42072d94451d457e4c6f1cd5a280dda
Costa Nonato <costa@nonato.com>
2018-10-24 18:35:56 -0300

    adding in-memory-database for categories

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,15 @@
+import { InMemoryDbService } from "angular-in-memory-web-api";
+
+export class InMemoryDatabase implements InMemoryDbService {
+  createDb(){
+    const categories = [
+      { id: 1, name: 'Moradia', description: 'Pagamentos de Contas da Casa' },
+      { id: 2, name: 'Saúde', description: 'Plano de Saúde e Remédios' },
+      { id: 3, name: 'Lazer', description: 'Cinema, parques, praia, etc' },
+      { id: 4, name: 'Salário', description: 'Recebimento de Salário'},
+      { id: 5, name: 'Freelas', description: 'Trabalhos como freelancer'}
+    ];
+
+    return { categories }
+  }
+}
