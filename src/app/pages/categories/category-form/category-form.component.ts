89f18144db960a5f02e87924f771c4918a2659db
Costa Nonato <costa@nonato.com>
2018-10-30 17:24:00 -0300

    improving CategoryFormComponent

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -40,6 +40,15 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked{
     this.setPageTitle();
   }
 
+  submitForm(){
+    this.submittingForm = true;
+
+    if(this.currentAction == "new")
+      this.createCategory();
+    else // currentAction == "edit"
+      this.updateCategory();
+  }
+
 
   // PRIVATE METHODS
 
@@ -82,5 +91,49 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked{
       const categoryName = this.category.name || ""
       this.pageTitle = "Editando Categoria: " + categoryName;
     }
+  }
+
+
+  private createCategory(){
+    const category: Category = Object.assign(new Category(), this.categoryForm.value);
+
+    this.categoryService.create(category)
+      .subscribe(
+        category => this.actionsForSuccess(category),
+        error => this.actionsForError(error)
+      )
+  }
+
+
+  private updateCategory(){
+    const category: Category = Object.assign(new Category(), this.categoryForm.value);
+
+    this.categoryService.update(category)
+      .subscribe(
+        category => this.actionsForSuccess(category),
+        error => this.actionsForError(error)
+      )
+  }
+
+  
+  private actionsForSuccess(category: Category){
+    toastr.success("Solicitação processada com sucesso!");
+
+    // redirect/reload component page
+    this.router.navigateByUrl("categories", {skipLocationChange: true}).then(
+      () => this.router.navigate(["categories", category.id, "edit"])
+    )
+  }
+
+
+  private actionsForError(error){
+    toastr.error("Ocorreu um erro ao processar a sua solicitação!");
+
+    this.submittingForm = false;
+
+    if(error.status === 422)
+      this.serverErrorMessages = JSON.parse(error._body).errors;
+    else
+      this.serverErrorMessages = ["Falha na comunicação com o servidor. Por favor, teste mais tarde."]
   }
 }
