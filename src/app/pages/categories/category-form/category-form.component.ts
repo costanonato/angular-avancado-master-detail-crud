81c7a7ce5058af7666794095a21ef0ec8ff50cb8
Costa Nonato <costa@nonato.com>
2018-11-01 10:04:35 -0300

    completing the category module

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -134,6 +134,6 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked{
     if(error.status === 422)
       this.serverErrorMessages = JSON.parse(error._body).errors;
     else
-      this.serverErrorMessages = ["Falha na comunicação com o servidor. Por favor, teste mais tarde."]
+      this.serverErrorMessages = ["Falha na comunicação com o servidor. Por favor, tente mais tarde."]
   }
 }
