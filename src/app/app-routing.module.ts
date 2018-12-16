dce47f68bb15180b6af65f8ab196a8f93d3f5c0a
Costa Nonato <costa@nonato.com>
2018-12-16 18:27:35 -0300

    adding default blank route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,9 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: 'entries', loadChildren: './pages/entries/entries.module#EntriesModule' },
   { path: 'categories', loadChildren: './pages/categories/categories.module#CategoriesModule' },
-  { path: 'reports', loadChildren: './pages/reports/reports.module#ReportsModule' }
+  { path: 'reports', loadChildren: './pages/reports/reports.module#ReportsModule' },
+
+  { path: '', redirectTo: '/reports', pathMatch: 'full' }
 ];
 
 @NgModule({
