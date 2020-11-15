d9455a70974e3cc068d72c891f4637276ff46480
Costa Nonato <costa@nonato.com>
2020-11-15 09:36:48 -0300

    updating angular version from 7 to 8

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'entries', loadChildren: './pages/entries/entries.module#EntriesModule' },
-  { path: 'categories', loadChildren: './pages/categories/categories.module#CategoriesModule' },
-  { path: 'reports', loadChildren: './pages/reports/reports.module#ReportsModule' },
+  { path: 'entries', loadChildren: () => import('./pages/entries/entries.module').then(m => m.EntriesModule) },
+  { path: 'categories', loadChildren: () => import('./pages/categories/categories.module').then(m => m.CategoriesModule) },
+  { path: 'reports', loadChildren: () => import('./pages/reports/reports.module').then(m => m.ReportsModule) },
 
   { path: '', redirectTo: '/reports', pathMatch: 'full' }
 ];
